b4967a5ecdf9bae49e1ff81536ff8ab5db981a64
Sam Steele <samsteele@User-Mac.local>
2017-08-25 15:01:16 -0400

    Working on express-validator line 90

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const mustache = require('mustache-express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
+const issymbol = require('issymbol');
 const fs = require('fs');
 const path = require('path');
 const app = express();
 const port = 3000;
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+const image = "/hangman-stick-figure.png";
+const image2 = "/hang-man.jpg";
 
 app.engine('mustache', mustache());
 app.set('views', './views');
@@ -32,11 +35,12 @@ let wordObjectString = "";
 let letter = "";
 let message = "";
 let difficulty = "";
+let endGameMessage ="";
+let endGameWin = [];
+let endGameLose = [];
 
 app.get('/', function(req, res){
-  // want to get difficulty selection (in a post?) - sets word length: 4-6, 6-8, 8+
-  // '/' should be a setup page
-  res.redirect('/index');
+  res.redirect('index');
 })
 
 app.get('/index', function(req, res){
@@ -44,31 +48,78 @@ app.get('/index', function(req, res){
   wrongGuessedLetters = [];
   letter = "";
   message = "";
-  res.render('index')
+
+  res.render('index', {image})
 })
 
 app.post('/setup', function(req, res){
   difficulty = req.body.gameDiff
+  // get style - select file of pictures
   req.session.tries = 8;
   tries = req.session.tries;
   req.session.word = genWordByLength(difficulty);
   res.redirect('game');
 })
 
+// check for a win or lose - delay on redirect
 app.get('/game', function(req,res) {
   if(req.session.tries===undefined){
-    res.redirect('/index');
+    res.redirect('index');
   }else{
-  wordObjectString = wordSmith(req.session.word, letter).join(" ");
-  res.render('game', {tries, theWord, wordObjectString, wrongGuessedLetters, message});
+    if(tries===0){
+      endGameMessage = "You lose."
+      endGameLose.push("something")
+      res.redirect('end');
+    }else{
+      wordObjectString = wordSmith(req.session.word, letter).join(" ");
+      if(!wordObjectString.includes("_")){
+        endGameMessage = "You win!!"
+        endGameWin.push('eh');
+        res.redirect('end');
+      }else{
+        // want to display "tries" as array of images
+      res.render('game', {tries, theWord, wordObjectString, wrongGuessedLetters, message});
+      }
+    }
   }
 })
 
 app.post('/guess', function(req, res){
-  // use validation to check for one letter - return error
+  // use validation to check for one letter, not numbers or symbols - return error
   letter = req.body.guessedLetter;
-  res.redirect('/game');
+  req.checkBody('guessedLetter', 'Must be only one letter').len(1,1);
+  req.checkBody('guessedLetter', 'Must be a letter').isAlpha();
+  // req.checkBody('letter', 'Must not be a number').isNaN();
+  // req.checkBody('letter', 'Must not be a symbol').issymbol();not
+  req.getValidationResult().then(function(result){
+    console.log(result.isEmpty);
+    console.log(result.array);
+    console.log(result.mapped);
+    console.log(result.throw);
+    message = result.throw;
+  })
+  res.redirect('game');
 })
+
+app.get('/end', function(req, res){
+  if(endGameMessage===undefined||endGameMessage===""){
+    res.redirect('index');
+  }else{
+    res.render('end', {endGameMessage, endGameWin, endGameLose, tries, difficulty, theWord, image2});
+  }
+})
+
+let name = "";
+app.post('/name', function(req, res){
+  name = req.body.name;
+  endGameMessage = "High scores page comming soon!"
+  // need to make .json file to hold high scores & pictures
+  res.redirect('end');
+ })
+
+ app.post('/restart', function(req, res){
+    res.redirect('index');
+  })
 
 function genWordByLength(difficulty){
   let sizeMin = 0;
@@ -83,10 +134,19 @@ function genWordByLength(difficulty){
   }
   if(difficulty==="hard"){
     sizeMin = 8;
+    sizeMax = 12;
+  }
+  if(difficulty==="xhard"){
+    sizeMin = 12;
+    sizeMax = 20;
+  }
+  if(difficulty==="nightmare"){
+    sizeMin = 20;
     sizeMax = 40;
   }
+  // want xhard and nightmare modes
   theWord = words[Math.floor(Math.random() * words.length)];
-  while(theWord.length<sizeMin||theWord.length>sizeMax){
+  while(theWord.length<=sizeMin||theWord.length>=sizeMax){
     theWord = words[Math.floor(Math.random() * words.length)];
   }
   return theWord;
@@ -96,19 +156,21 @@ function wordSmith(word, char){
   let theWordArray = Array.from(word);
   let theModWordArray = [];
   if(char!=""){
-    if(theWordArray.includes(char)){
+    if(theWordArray.includes(char)&&!correctGuessedLetters.includes(char)){
       correctGuessedLetters.push(char);
+      message = "";
     }else{
-      if(wrongGuessedLetters.includes(" "+char)){
-        message = "You already tried that letter. You are wasting tries, you idiot!";
+      if(wrongGuessedLetters.includes(" "+char)||correctGuessedLetters.includes(char)){
+        message = "You already tried that letter, you idiot!";
       }
       else{
         message = "";
+        wrongGuessedLetters.push(" "+char);
+        char = "";
+        tries -= 1;
       }
-      wrongGuessedLetters.push(" "+char);
-      char = "";
-      tries -= 1;
     }
+    console.log(correctGuessedLetters.length);
   }
 
   theWordArray.forEach(function(currentValue) {
